faf291a2b39649aadd8bd27329554f7cfc4249a9
Anshika <85496946+groot1302@users.noreply.github.com>
2022-02-03 20:42:59 +0530

    Add files via upload

---
+++ max_value.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    
+    printf("How many values do you want to enter in array?");
+    scanf("%d",&n);
+    int a[n];
+    int sum=0;
+    printf("ENter the values\n");
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    
+    int max=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(max<a[i])
+        max=a[i];
+    }
+    printf("The maximum number  is %d",max);
+}
